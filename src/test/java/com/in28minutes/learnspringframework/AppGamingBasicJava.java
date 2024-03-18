7b03bb4fd651dec6ef42e2082889dfd570089dab
Sehee Yoon <125191604+ysehee@users.noreply.github.com>
2024-03-18 14:24:22 +0900

    test2

--- src/test/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ src/test/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -1,5 +1,13 @@
 package src.test.java.com.in28minutes.learnspringframework;
 
 public class AppGamingBasicJava {
-    
+    public static void main(String[] args) {
+      var gameRunner = new GameRunner();
+      var marioGame = new MarioGame();
+
+    }
+
+
+
+
 }
